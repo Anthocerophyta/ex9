3dff3b256b7ebc6a42bf410e7bf567c21cd1b946
Anthocerophyta <love.tom.100.mag@gmail.com>
2019-02-11 23:21:35 +0700

    edit1

---
+++ 2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<string>
+using namespace std;
+
+int main(){
+	string data;
+	ifstream import("F:\\Comprograming\\ex9\\2.txt");
+	float a,b,y=0;
+	int n;
+	cout<<"Enter min - max :";
+	cin>>a>>b;
+	while(getline(import,data)){
+		y=atof(data.c_str());
+		if(y>=a&&y<=b){
+			n++;
+		}
+	}
+	if(n==0) cout<<"Invalid input. (max>=min)";
+	else cout<<"Number of student:"<<n;
+	
+}
